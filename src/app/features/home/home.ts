3875a8aaf0a3e3c5f33eb6afd3c8eb4c9db39f2f
Gabriel Albinet <gabriel.albinet.pro@gmail.com>
2026-07-21 14:38:45 +0200

    stats de la page home récupérées sur l'API

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {Component, OnInit, signal, inject} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {CharactersService} from '../../features/characters/services/characters';
+import {LocationsService} from '../../features/locations/services/locations';
+import {EpisodesService} from '../../features/episodes/services/episodes';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +10,26 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  private readonly charactersService = inject(CharactersService);
+  private readonly locationsService = inject(LocationsService);
+  private readonly episodesService = inject(EpisodesService);
+
+  charactersCount = signal(0);
+  locationsCount = signal(0);
+  episodesCount = signal(0);
+
+  ngOnInit() {
+    this.charactersService.getCharacterFromComponent().subscribe((response) => {
+      this.charactersCount.set(response.info.count);
+    });
+
+    this.locationsService.getLocationFromComponent().subscribe((response) => {
+      this.locationsCount.set(response.info.count);
+    });
+
+    this.episodesService.getEpisodeFromComponent().subscribe((response) => {
+      this.episodesCount.set(response.info.count);
+    });
+  }
+}
